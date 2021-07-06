bbf2a16c01d534bd07725173b1b932b2998321e5
Rafa Hinojosa <rafael.zeo@gmail.com>
2021-07-06 17:52:41 -0500

    3. User can Log Out from MainActivity

--- Instagram/app/src/main/java/com/example/instagram/MainActivity.java
+++ Instagram/app/src/main/java/com/example/instagram/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.instagram.databinding.ActivityLoginBinding;
+import com.example.instagram.databinding.ActivityMainBinding;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Binding
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // Associate variables with components in activity_main.xml
+        btnLogout = binding.btnLogout;
+        // On Click Listener to Log Out
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // If user clicks, Logs Out and goes back (through an intent) to LoginActivity
+                ParseUser.logOut();
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
